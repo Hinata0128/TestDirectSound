5ee912ac4cf553a16ad4825dd036a82b46d1e637
game <game@R200-18>
2025-08-04 14:19:20 +0900

    CSoundManager完了

--- Run/Source/Sound/CSoundManager.cpp
+++ Run/Source/Sound/CSoundManager.cpp
@@ -148,6 +148,5 @@ void CSoundManager::SetVolume(std::string list, int volume)
 //Ä¶’†‚©.
 bool CSoundManager::Playing(std::string list)
 {
-	//...
 	return GetInstance()->m_pDxSound[list]->Playing();
 }
